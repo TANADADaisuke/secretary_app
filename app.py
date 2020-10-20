9079446ef7e5c56e150686e815a68078634196e4
Tanada Daisuke <nvkx873110@gmail.com>
2020-10-21 00:13:24 +0900

    status checker

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import csv
 import time
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def print_pause(text):
@@ -11,6 +11,9 @@ def print_pause(text):
 def announce_task_registration(task, due):
     print('>>> 新しいタスクが登録されました')
     print('{}(期日: {})'.format(task, due))
+
+def format_datetime(date_time):
+    return date_time.isoformat(timespec='milliseconds')
 
 
 # greetings
@@ -29,7 +32,7 @@ with open('tasks.csv', newline='') as csvfile:
 
 # register new task
 with open('tasks.csv', 'a', newline='') as csvfile:
-    fieldnames = ['id', 'tasks', 'registerd', 'due']
+    fieldnames = ['id', 'tasks', 'registerd', 'due', 'status']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     # writer.writeheader()
 
@@ -39,12 +42,25 @@ with open('tasks.csv', 'a', newline='') as csvfile:
     due_time = input('期限: ')
     # automatically filled form
     new_id = current_id + 1
-    registered_time = datetime.utcnow().isoformat(timespec='milliseconds')
+    registered_time = datetime.utcnow()
+    # set task status
+    new_time = datetime.utcnow()
+    delta_time = new_time - registered_time
+    delta_green = timedelta(days=2)
+    delta_yellow = timedelta(days=5)
+    if delta_time <= delta_green:
+        status = 'green'
+    elif delta_time <= delta_yellow:
+        status = 'yellow'
+    else:
+        status = 'red'
+    # write row
     writer.writerow({
         'id': new_id,
         'tasks': new_task,
-        'registerd': registered_time,
-        'due': due_time
+        'registerd': format_datetime(registered_time),
+        'due': due_time,
+        'status': status
     })
 
     # announce successfully registerd
